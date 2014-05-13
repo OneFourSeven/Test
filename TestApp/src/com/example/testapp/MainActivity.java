75e87471f58ed3fa9f60ce75724dcdebe64931de
Apurv <apurvjpatel@gmail.com>
2014-05-14 00:19:46 +0530

    Camera! :D

--- TestApp/src/com/example/testapp/MainActivity.java
+++ TestApp/src/com/example/testapp/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.testapp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 import android.os.Build;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
 
 public class MainActivity extends ActionBarActivity {
 
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	private Uri fileUri;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -59,6 +76,62 @@ public class MainActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+	
+	public void getImage(View view){
+		
+		// create Intent to take a picture and return control to the calling application
+	    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+	    //fileUri = new Uri(); // create a file to save the image
+	    //ContentValues values = new ContentValues(3);  
+        //values.put(MediaStore.Images.Media.DISPLAY_NAME, "testing");  
+        //values.put(MediaStore.Images.Media.DESCRIPTION, "this is description");  
+        //values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");  
+        //Uri imageFilePath = MainActivity.this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);  
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFilePath); 
+        
+	    //intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFilePath); // set the image file name
+
+	    File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+	    
+	    File output = new File(dir,"gtumca.png");
+	    //intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(output));
+	    String path = output.getAbsolutePath(); 
+	    
+	    Toast.makeText(this, path, Toast.LENGTH_LONG).show();
+	    // start the image capture Intent
+	    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // Image captured and saved to fileUri specified in the Intent
+	            
+	            ImageView imageView = (ImageView)this.findViewById(R.id.imageView1);
+	            Bitmap photo = (Bitmap) data.getExtras().get("data"); 
+	            imageView.setImageBitmap(photo);
+	            
+	            Uri tempUri = getImageUri(getApplicationContext(), photo);
+	            
+	            Toast.makeText(this, "Image saved to:\n" +
+	                     tempUri.toString(), Toast.LENGTH_LONG).show();
+	            
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
+	}
+	
+	public Uri getImageUri(Context inContext, Bitmap inImage) {
+	    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+	    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+	    String path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+	    return Uri.parse(path);
 	}
 
 }
